e5d02b4fba96076490574787f600e89e9c6a8eff
Jetsadawut Thungmuangthong <61123824@g.cmru.ac.th>
2021-02-13 13:30:59 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,22 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']) ;
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [HomeController::class, 'about']) ;
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [HomeController::class, 'contact']) ;
 
 Route::get('/สวัสดี', function () {
     return "สวัสดี";
 });
 
 Route::get('/hello/{min}', function ($name) {
-    return "สวัสดี $name";
+return "สวัสดี $name";
+});
+
+Route::get('/calculater/{num1}/{num2}', function ($num1, $num2) {
+    return "ผลบวกของเลข $num1 และ $num2 คือ " . ($num1 + $num2) ;
 });

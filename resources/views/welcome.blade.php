68eecbdaf4847002c850d078ad4e4e1d6c49f3ea
Jetsadawut Thungmuangthong <61123824@g.cmru.ac.th>
2021-02-13 15:22:41 +0700

    สร้าง user ได้แล้ว

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=h1, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="/">หน้าแรก</a> |  <a href="/about">เกี่ยวกับ</a> |  <a href="/contact">ติดต่อ</a>
+@extends('layout.master')
+
+@section('content')
     <h1>สวัสดี Laravel</h1>
-    <hr>
-    &copy; 2021 Jetsadawut Thungmuangthong
-</body>
-</html>
+@endsection
